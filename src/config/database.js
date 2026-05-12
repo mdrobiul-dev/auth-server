28faf74a78acf9767da5b97cc8935d039fbedb97
its-robii <robiulhassanrobi33@gmail.com>
2026-05-12 13:58:13 +0600

    environment and configaration such as env mongodb database connect

---
+++ src/config/database.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import config from "./index";
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(config.mongoDbUrl);
+    console.log("Mongodb is connected successfully");
+  } catch (error) {
+    console.error(`mongodb connection failed ${error.message}`);
+    process.exit(1);
+  }
+};
+
+// gracefull shutdown
+
+process.on("SIGINT", async () => {
+    await mongoose.connection.close()
+    console.log("Mongo db connection closed")
+    process.exit(0)
+})
+
+export default connectDb
---
+++ src/config/index.js
@@ -0,0 +1,19 @@
+import dotenv, { config } from "dotenv"
+import path from "path"
+
+dotenv.config({path : path.resolve(process.cwd(), ".env")})
+
+
+const config = {
+    env = process.env.NODE_ENV || "development",
+    port = process.env.PORT || 5000,
+    mongoDbUrl = process.env.MONGO_URI,
+
+    jwt : {
+        secret = process.env.JWT_SECRET,
+        expireIN = process.env.JWT_EXPIRES_IN || "1d"
+    },
+    bycryptSaltRound = parseInt(process.env.BCRYPT_SALT_ROUNDS) || 12
+}
+
+export default config
