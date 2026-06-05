a07f1fbbcac499a3260f4db10358e6c72e2c82bd
its-robii <robiulhassanrobi33@gmail.com>
2026-06-05 19:08:49 +0600

    refreshtoken added

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,14 +1,23 @@
-import express from "express"
-import { loginSchema, registerSchema, validate } from "../validators/auth.validator.js"
-import { login, logout, refreshToken, register } from "../controllers/auth.controller.js"
+import express from "express";
+import {
+  loginSchema,
+  registerSchema,
+  validate,
+} from "../validators/auth.validator.js";
+import {
+  login,
+  logout,
+  refreshToken,
+  register,
+} from "../controllers/auth.controller.js";
 import { authLimiter } from "../middleware/rateLimiter.middleware.js";
 import { protect } from "../middleware/auth.middleware.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post("/register",authLimiter, validate(registerSchema), register)
-router.post("/login",authLimiter, validate(loginSchema), login)
-router.post('/refresh-token', refreshToken);
-router.post('/logout', protect, logout);
+router.post("/register", authLimiter, validate(registerSchema), register);
+router.post("/login", authLimiter, validate(loginSchema), login);
+router.post("/refresh-token", refreshToken);
+router.post("/logout", protect, logout);
 
-export default router
+export default router;
